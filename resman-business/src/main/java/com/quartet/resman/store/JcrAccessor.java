b5bdb2f54fdc9b3f0273d77184cd1f627d9ac95d
lcheng <chengluren@126.com>
2014-12-26 11:26:45 +0800

    初步添加jackrabbit支持

--- resman-business/src/main/java/com/quartet/resman/store/JcrAccessor.java
+++ resman-business/src/main/java/com/quartet/resman/store/JcrAccessor.java
@@ -1,16 +1,16 @@
 package com.quartet.resman.store;
 
 import org.apache.jackrabbit.JcrConstants;
+import org.apache.jackrabbit.commons.JcrUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.extensions.jcr.JcrCallback;
 import org.springframework.extensions.jcr.JcrTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.jcr.Node;
-import javax.jcr.RepositoryException;
-import javax.jcr.Session;
+import javax.jcr.*;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * @author lcheng
@@ -29,6 +29,7 @@ public class JcrAccessor {
             public String doInJcr(Session session) throws IOException, RepositoryException {
                 Node root = session.getRootNode();
                 Node node = root.addNode(name, JcrConstants.NT_FOLDER);
+                session.save();
                 return node.getPath();
             }
         });
@@ -42,17 +43,13 @@ public class JcrAccessor {
                 Node parentNode = root.getNode(parent);
                 if (parentNode != null) {
                     Node node = parentNode.addNode(name, JcrConstants.NT_FOLDER);
+                    session.save();
                     return node.getPath();
                 }
                 return null;
             }
         });
     }
-
-//    public void getChildren(String parent){
-//         Node root = jcrTemplate.getRootNode();
-//
-//    }
 
     public String addFile(final String folder, final InputStream is, final String fileName, final String mimeType) {
         return jcrTemplate.execute(new JcrCallback<String>() {
@@ -62,11 +59,56 @@ public class JcrAccessor {
                 Node parentNode = root.getNode(folder);
                 if (parentNode != null) {
                     Node fileNode = parentNode.addNode(fileName, JcrConstants.NT_FILE);
-                    fileNode.setProperty(JcrConstants.JCR_MIMETYPE, mimeType);
-                    fileNode.setProperty(JcrConstants.JCR_DATA, session.getValueFactory().createBinary(is));
+                    Node resNode = fileNode.addNode(JcrConstants.JCR_CONTENT, JcrConstants.NT_RESOURCE);
+                    resNode.setProperty(JcrConstants.JCR_MIMETYPE, mimeType);
+                    resNode.setProperty(JcrConstants.JCR_DATA, session.getValueFactory().createBinary(is));
+                    session.save();
                     return fileNode.getPath();
                 }
                 return null;
+            }
+        });
+    }
+
+    public InputStream readFile(final String path) {
+        return jcrTemplate.execute(new JcrCallback<InputStream>() {
+            @Override
+            public InputStream doInJcr(Session session) throws IOException, RepositoryException {
+                Node root = session.getRootNode();
+                Node fileNode = root.getNode(path);
+                if (fileNode!=null){
+                    return JcrUtils.readFile(fileNode);
+                }
+                return null;
+            }
+        });
+    }
+
+    public void readFile(final String path,final OutputStream os){
+        jcrTemplate.execute(new JcrCallback<Boolean>() {
+            @Override
+            public Boolean doInJcr(Session session) throws IOException, RepositoryException {
+                Node root = session.getRootNode();
+                Node fileNode = root.getNode(path);
+                if (fileNode!=null){
+                    JcrUtils.readFile(fileNode,os);
+                }
+                return true;
+            }
+        });
+    }
+
+    public void deleteNode(final String path) {
+        jcrTemplate.execute(new JcrCallback<Boolean>() {
+            @Override
+            public Boolean doInJcr(Session session) throws IOException, RepositoryException {
+                Node root = session.getRootNode();
+                NodeIterator iterator = root.getNodes(path);
+                while (iterator.hasNext()) {
+                    iterator.nextNode().remove();
+                }
+                session.save();
+                return true;
             }
         });
     }
--- resman-business/src/test/java/com/quartet/resman/store/JcrAccessorTest.java
+++ resman-business/src/test/java/com/quartet/resman/store/JcrAccessorTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.util.Assert;
 
+import java.io.*;
+
 /**
  * @author lcheng
  * @version 1.0
@@ -20,9 +22,68 @@ public class JcrAccessorTest {
 
     @Test
     public void testAddFolder(){
+        accessor.deleteNode("精品课");
         String p1 = accessor.addFolder("精品课");
-        String p2 = accessor.addFolder("/精品课","高等数学");
+        String p2 = accessor.addFolder("精品课","高等数学");
         Assert.notNull(p1);
         Assert.notNull(p2);
+        accessor.deleteNode("精品课");
+    }
+
+    @Test
+    public void testAddFile(){
+        accessor.deleteNode("精品课");
+        String p1 = accessor.addFolder("精品课");
+        String p2 = accessor.addFolder("精品课","高等数学");
+        try(InputStream is = new FileInputStream(new File("D:/cluster.log"))){
+             String path = accessor.addFile("精品课/高等数学",is,"cluster.log","");
+            System.out.println(path);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testAddAndReadFile(){
+        accessor.deleteNode("精品课");
+        String p1 = accessor.addFolder("精品课");
+        String p2 = accessor.addFolder("精品课","高等数学");
+        try(InputStream is = new FileInputStream(new File("D:/cluster.log"))){
+            String path = accessor.addFile("精品课/高等数学",is,"cluster.log","");
+            System.out.println(path);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        InputStream is = accessor.readFile("精品课/高等数学/cluster.log");
+
+        byte[] buffer = new byte[1024];
+        int read =-1;
+        try(OutputStream os = new FileOutputStream(new File("D:/readOut.log"))){
+            while((read = is.read(buffer))>0){
+                os.write(buffer,0,read);
+            }
+            is.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testAddAndReadFile2(){
+        accessor.deleteNode("精品课");
+        String p1 = accessor.addFolder("精品课");
+        String p2 = accessor.addFolder("精品课","高等数学");
+        try(InputStream is = new FileInputStream(new File("D:/cluster.log"))){
+            String path = accessor.addFile("精品课/高等数学",is,"cluster.log","");
+            System.out.println(path);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        try(OutputStream os = new FileOutputStream(new File("D:/readOut.log"))){
+            accessor.readFile("精品课/高等数学/cluster.log",os);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
